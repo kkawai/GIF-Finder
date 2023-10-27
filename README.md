9f7afe92898c59d0a560f64eace903aa2f451a58
kkawai <kkawai2@gmail.com>
2023-10-27 12:07:14 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GIF-Finder
 Find GIF images and share with friends.  super fun
+
+
+https://play.google.com/store/apps/details?id=com.kk.android.fuzzy_waddle
---
+++ app/src/main/java/com/kk/android/fuzzy_waddle/GiphyGifFragment.java
@@ -0,0 +1,46 @@
+package com.kk.android.fuzzy_waddle;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kk.android.fuzzy_waddle.model.GiphyImage;
+import com.kk.android.fuzzy_waddle.net.GiphyGifLoader;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+import androidx.loader.content.Loader;
+
+public class GiphyGifFragment extends GiphyFragment {
+
+  @Override
+  public @NonNull
+  Loader<List<GiphyImage>> onCreateLoader(int id, Bundle args) {
+    return new GiphyGifLoader(getActivity(), searchString);
+  }
+
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup viewGroup, Bundle bundle) {
+    View view = super.onCreateView(inflater, viewGroup, bundle);
+    ((SearchView)view.findViewById(R.id.gifSearchBar))
+            .setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+              @Override
+              public boolean onQueryTextSubmit(String query) {
+                if (query.length() < 2) {
+                  return false;
+                }
+                setSearchString(query);
+                return false;
+              }
+
+              @Override
+              public boolean onQueryTextChange(String newText) {
+                return false;
+              }
+            });
+    return view;
+  }
+}
