35d0f3b1df9fdd24594d27144147d6a2143620b1
kkawai <kkawai2@gmail.com>
2023-10-30 01:42:39 -0700

    added privacy policy to app

--- app/src/main/java/com/kk/android/fuzzy_waddle/GiphyGifFragment.java
+++ app/src/main/java/com/kk/android/fuzzy_waddle/GiphyGifFragment.java
@@ -1,7 +1,12 @@
 package com.kk.android.fuzzy_waddle;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,33 +19,42 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
 import androidx.loader.content.Loader;
 
-public class GiphyGifFragment extends GiphyFragment {
+public class GiphyGifFragment extends GiphyFragment implements View.OnClickListener {
 
-  @Override
-  public @NonNull
-  Loader<List<GiphyImage>> onCreateLoader(int id, Bundle args) {
-    return new GiphyGifLoader(getActivity(), searchString);
-  }
+    public static final String PRIVACY_POLICY_URL = "https://raw.githack.com/kkawai/GIF-Finder/master/external/privacy_policy.html";
 
-  @Override
-  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup viewGroup, Bundle bundle) {
-    View view = super.onCreateView(inflater, viewGroup, bundle);
-    ((SearchView)view.findViewById(R.id.gifSearchBar))
-            .setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-              @Override
-              public boolean onQueryTextSubmit(String query) {
-                if (query.length() < 2) {
-                  return false;
-                }
-                setSearchString(query);
-                return false;
-              }
+    @Override
+    public @NonNull
+    Loader<List<GiphyImage>> onCreateLoader(int id, Bundle args) {
+        return new GiphyGifLoader(getActivity(), searchString);
+    }
 
-              @Override
-              public boolean onQueryTextChange(String newText) {
-                return false;
-              }
-            });
-    return view;
-  }
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup viewGroup, Bundle bundle) {
+        View view = super.onCreateView(inflater, viewGroup, bundle);
+        ((SearchView) view.findViewById(R.id.gifSearchBar))
+                .setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        if (query.length() < 2) {
+                            return false;
+                        }
+                        setSearchString(query);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        return false;
+                    }
+                });
+        view.findViewById(R.id.privacy_policy).setOnClickListener(this);
+        return view;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PRIVACY_POLICY_URL)));
+    }
 }
