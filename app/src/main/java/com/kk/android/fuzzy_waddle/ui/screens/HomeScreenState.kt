ed0ebf84ecba4c04b1e0b073c638b8aa615ea0cd
kkawai <kkawai2@gmail.com>
2023-11-20 20:44:34 -0800

    minor changes

--- app/src/main/java/com/kk/android/fuzzy_waddle/ui/screens/HomeScreenState.kt
+++ app/src/main/java/com/kk/android/fuzzy_waddle/ui/screens/HomeScreenState.kt
@@ -2,7 +2,7 @@ package com.kk.android.fuzzy_waddle.ui.screens
 
 import com.kk.android.fuzzy_waddle.model.GiphyImage
 
-data class HomeScreenState (
+    data class HomeScreenState (
     val isLoading: Boolean = false,
     var gifImages: List<GiphyImage> = emptyList(),
     val error: String = "",
